ae1eb82366ecc230f4883027738fb84f7f01cf04
Reda EL HAIL <elhailreda@gmail.com>
2020-12-30 12:00:34 +0100

    add viewsets to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
+
+router = DefaultRouter()
+router.register('hello-viewset',views.HelloViewset,basename='hello-viewset')
+
+
 urlpatterns = [
-path('hello/',views.HelloApiView.as_view())
+    path('hello/',views.HelloApiView.as_view()),
+	path('',include(router.urls))
+
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -47,4 +47,46 @@ class HelloApiView(APIView):
         return Response({'method':'DELETE'})
 
 
-class
+class HelloViewset(viewsets.ViewSet):
+    """Test API Viewset"""
+
+    serializer_class = serializers.HelloSerializer
+
+
+    def list(self,request,):
+        """Return hello message"""
+
+        a_viewset = [
+             'Uses actions (list,create,retrieve,update,partial_update)',
+             'Automatically maps to urls using Routers',
+             'Provides more functionality with less code'
+        ]
+
+        return Response({'message':'Hello!', 'a_viewset':'a_viewset'})
+
+    def create(self,request):
+        """create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self,request,pk=None):
+        """Handle retrieving an object"""
+        return Response({'method':'GET'})
+
+    def update(self,request,pk=None):
+        """Handle updating an object"""
+        return Response({'method':'PUT'})
+
+    def partial_update(self,request,pk=None):
+        """Handle updating partially an object"""
+        return Response({'method':'PATCH'})
+    def destroy(self,request,pk=None):
+        """Handle deleting an object"""
+        return Response({'method':'DELETE'})
